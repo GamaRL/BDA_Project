b2e0d72e47b2e5a82a6f36536155e9bfdd7fd5a4
yosoyluisarias21@gmail.com <luis@pc-bda-laq.fi.unam>
2024-12-03 15:37:58 -0600

    Corrección de tablespaces

--- scripts/s14-crea-tablespaces-ordinario.sql
+++ scripts/s14-crea-tablespaces-ordinario.sql
@@ -14,18 +14,18 @@ connect sys/system3@proveedores as sysdba;
 -- Si existen los tablespaces, los elimina
 drop tablespace if exists proveedores_indexes_tbs including contents and datafiles;
 drop tablespace if exists proveedores_blob_tbs including contents and datafiles;
-drop tablespace if exists proveedores_default_tbs including contents and datafiles;
-drop tablespace if exists proveedores_cuenta_bancaria_tbs including contents and datafiles;
+drop tablespace if exists proveedores_data_tbs including contents and datafiles;
+drop tablespace if exists proveedores_c_bancaria_tbs including contents and datafiles;
 
 --
 -- Tablespace: proveedores_indexes_tbs
 --
 create tablespace proveedores_indexes_tbs
     datafile
-        '/unam/bda/proyecto/disks/d04/proveedores_indexes_01.dbf' size 100m,
-        '/unam/bda/proyecto/disks/d05/proveedores_indexes_02.dbf' size 100m,
-        '/unam/bda/proyecto/disks/d06/proveedores_indexes_03.dbf' size 100m
-    autoextend on next 50m maxsize 450m
+        '/unam/bda/proyecto/disks/d04/proveedores_indexes_01.dbf' size 50m,
+        '/unam/bda/proyecto/disks/d05/proveedores_indexes_02.dbf' size 50m,
+        '/unam/bda/proyecto/disks/d06/proveedores_indexes_03.dbf' size 50m
+    autoextend on next 1m maxsize 250m
     extent management local autoallocate
     segment space management auto
     default
@@ -36,8 +36,8 @@ create tablespace proveedores_indexes_tbs
 -- Tablespace: proveedores_blob_tbs
 --
 create bigfile tablespace proveedores_blob_tbs
-    datafile '/unam/bda/proyecto/disks/d07/proveedores_blob_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d07/proveedores_blob_01.dbf' size 500m
+    autoextend on next 5m maxsize 1g
     extent management local autoallocate
     segment space management auto
 ;
@@ -46,18 +46,18 @@ create bigfile tablespace proveedores_blob_tbs
 -- Tablespace: proveedores_data_tbs
 --
 create bigfile tablespace proveedores_data_tbs
-    datafile '/unam/bda/proyecto/disks/d08/proveedores_data_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d08/proveedores_data_01.dbf' size 500m
+    autoextend on next 5m maxsize 750m
     extent management local autoallocate
     segment space management auto
 ;
 
 --
--- Tablespace: proveedores_cuenta_bancaria_tbs
+-- Tablespace: proveedores_c_bancaria_tbs
 --
-create bigfile tablespace proveedores_cuenta_bancaria_tbs
-    datafile '/unam/bda/proyecto/disks/d08/proveedores_cuenta_bancaria_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+create bigfile tablespace proveedores_c_bancaria_tbs
+    datafile '/unam/bda/proyecto/disks/d08/proveedores_c_bancaria_01.dbf' size 500m
+    autoextend on next 5m maxsize 750m
     extent management local autoallocate
     segment space management auto
 ;
@@ -78,17 +78,17 @@ drop tablespace if exists clientes_blob_tbs including contents and datafiles;
 drop tablespace if exists clientes_hash_1_tbs including contents and datafiles;
 drop tablespace if exists clientes_hash_2_tbs including contents and datafiles;
 drop tablespace if exists clientes_hash_3_tbs including contents and datafiles;
-drop tablespace if exists clientes_tarjeta_cliente_tbs including contents and datafiles;
+drop tablespace if exists clientes_tarjeta_tbs including contents and datafiles;
 
 --
 -- Tablespace: clientes_indexes_tbs
 --
 create tablespace clientes_indexes_tbs
     datafile
-        '/unam/bda/proyecto/disks/d09/clientes_indexes_01.dbf' size 100m,
-        '/unam/bda/proyecto/disks/d10/clientes_indexes_02.dbf' size 100m,
-        '/unam/bda/proyecto/disks/d11/clientes_indexes_03.dbf' size 100m
-    autoextend on next 50m maxsize 450m
+        '/unam/bda/proyecto/disks/d09/clientes_indexes_01.dbf' size 50m,
+        '/unam/bda/proyecto/disks/d10/clientes_indexes_02.dbf' size 50m,
+        '/unam/bda/proyecto/disks/d11/clientes_indexes_03.dbf' size 50m
+    autoextend on next 1m maxsize 250m
     extent management local autoallocate
     segment space management auto
     default
@@ -99,8 +99,8 @@ create tablespace clientes_indexes_tbs
 -- Tablespace: clientes_blob_tbs
 --
 create bigfile tablespace clientes_blob_tbs
-    datafile '/unam/bda/proyecto/disks/d12/clientes_blob_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d12/clientes_blob_01.dbf' size 500m
+    autoextend on next 5m maxsize 1g
     extent management local autoallocate
     segment space management auto
 ;
@@ -109,8 +109,8 @@ create bigfile tablespace clientes_blob_tbs
 -- Tablespace: clientes_hash_1_tbs
 --
 create bigfile tablespace clientes_hash_1_tbs
-    datafile '/unam/bda/proyecto/disks/d13/clientes_hash_1_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d13/clientes_hash_1_01.dbf' size 500m
+    autoextend on next 5m maxsize 750m
     extent management local autoallocate
     segment space management auto
     default
@@ -121,8 +121,8 @@ create bigfile tablespace clientes_hash_1_tbs
 -- Tablespace: clientes_hash_2_tbs
 --
 create bigfile tablespace clientes_hash_2_tbs
-    datafile '/unam/bda/proyecto/disks/d14/clientes_hash_2_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d14/clientes_hash_2_01.dbf' size 500m
+    autoextend on next 5m maxsize 750m
     extent management local autoallocate
     segment space management auto
     default
@@ -133,8 +133,8 @@ create bigfile tablespace clientes_hash_2_tbs
 -- Tablespace: clientes_hash_3_tbs
 --
 create bigfile tablespace clientes_hash_3_tbs
-    datafile '/unam/bda/proyecto/disks/d15/clientes_hash_3_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d15/clientes_hash_3_01.dbf' size 500m
+    autoextend on next 5m maxsize 750m
     extent management local autoallocate
     segment space management auto
     default
@@ -142,11 +142,11 @@ create bigfile tablespace clientes_hash_3_tbs
 ;
 
 --
--- Tablespace: clientes_tarjeta_cliente_tbs
+-- Tablespace: clientes_tarjeta_tbs
 --
-create bigfile tablespace clientes_tarjeta_cliente_tbs
-    datafile '/unam/bda/proyecto/disks/d16/clientes_tarjeta_cliente_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+create bigfile tablespace clientes_tarjeta_tbs
+    datafile '/unam/bda/proyecto/disks/d16/clientes_tarjeta_01.dbf' size 500m
+    autoextend on next 5m maxsize 750m
     extent management local autoallocate
     segment space management auto
 ;
@@ -164,9 +164,9 @@ connect sys/system3@servicios as sysdba;
 -- Si existen los tablespaces, los elimina
 drop tablespace if exists servicios_indexes_tbs including contents and datafiles;
 drop tablespace if exists servicios_blob_tbs including contents and datafiles;
-drop tablespace if exists servicios_default_tbs including contents and datafiles;
+drop tablespace if exists servicios_data_tbs including contents and datafiles;
 drop tablespace if exists servicios_historico_tbs including contents and datafiles;
-drop tablespace if exists servicios_servicio_contratacion_tbs including contents and datafiles;
+drop tablespace if exists servicios_contratacion_tbs including contents and datafiles;
 drop tablespace if exists servicios_transacciones_tbs including contents and datafiles;
 
 --
@@ -174,10 +174,10 @@ drop tablespace if exists servicios_transacciones_tbs including contents and dat
 --
 create tablespace servicios_indexes_tbs
     datafile
-        '/unam/bda/proyecto/disks/d17/servicios_indexes_17.dbf' size 100m,
-        '/unam/bda/proyecto/disks/d18/servicios_indexes_18.dbf' size 100m,
-        '/unam/bda/proyecto/disks/d19/servicios_indexes_19.dbf' size 100m
-    autoextend on next 50m maxsize 450m
+        '/unam/bda/proyecto/disks/d17/servicios_indexes_17.dbf' size 50m,
+        '/unam/bda/proyecto/disks/d18/servicios_indexes_18.dbf' size 50m,
+        '/unam/bda/proyecto/disks/d19/servicios_indexes_19.dbf' size 50m
+    autoextend on next 1m maxsize 250m
     extent management local autoallocate
     segment space management auto
     default
@@ -188,8 +188,8 @@ create tablespace servicios_indexes_tbs
 -- Tablespace: servicios_blob_tbs
 --
 create bigfile tablespace servicios_blob_tbs
-    datafile '/unam/bda/proyecto/disks/d20/servicios_blob_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d20/servicios_blob_01.dbf' size 500m
+    autoextend on next 5m maxsize 1g
     extent management local autoallocate
     segment space management auto
 ;
@@ -198,8 +198,8 @@ create bigfile tablespace servicios_blob_tbs
 -- Tablespace: servicios_data_tbs
 --
 create bigfile tablespace servicios_data_tbs
-    datafile '/unam/bda/proyecto/disks/d21/servicios_data_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d21/servicios_data_01.dbf' size 500m
+    autoextend on next 5m maxsize 750m
     extent management local autoallocate
     segment space management auto
 ;
@@ -209,25 +209,25 @@ create bigfile tablespace servicios_data_tbs
 --
 create tablespace servicios_historico_tbs
     datafile
-        '/unam/bda/proyecto/disks/d22/servicios_historico_01.dbf' size 333m,
-        '/unam/bda/proyecto/disks/d23/servicios_historico_02.dbf' size 333m,
-        '/unam/bda/proyecto/disks/d24/servicios_historico_03.dbf' size 334m
-    autoextend on next 100m maxsize 2g
+        '/unam/bda/proyecto/disks/d22/servicios_historico_01.dbf' size 200m,
+        '/unam/bda/proyecto/disks/d23/servicios_historico_02.dbf' size 200m,
+        '/unam/bda/proyecto/disks/d24/servicios_historico_03.dbf' size 200m
+    autoextend on next 10m maxsize 1g
     extent management local autoallocate
     segment space management auto
     default
-        table compress for archive low
+        table compress for oltp
 ;
 
 --
--- Tablespace: servicios_servicio_contratacion_tbs
+-- Tablespace: servicios_contratacion_tbs
 --
-create tablespace servicios_servicio_contratacion_tbs
+create tablespace servicios_contratacion_tbs
     datafile 
-        '/unam/bda/proyecto/disks/d25/servicios_servicio_contratacion_01.dbf' size 333m,
-        '/unam/bda/proyecto/disks/d26/servicios_servicio_contratacion_02.dbf' size 333m,
-        '/unam/bda/proyecto/disks/d27/servicios_servicio_contratacion_03.dbf' size 334m
-    autoextend on next 100m maxsize 2g
+        '/unam/bda/proyecto/disks/d25/servicios_servicio_contratacion_01.dbf' size 200m,
+        '/unam/bda/proyecto/disks/d26/servicios_servicio_contratacion_02.dbf' size 200m,
+        '/unam/bda/proyecto/disks/d27/servicios_servicio_contratacion_03.dbf' size 200m
+    autoextend on next 10m maxsize 1g
     extent management local autoallocate
     segment space management auto
     default
@@ -238,8 +238,8 @@ create tablespace servicios_servicio_contratacion_tbs
 -- Tablespace: servicios_transacciones_tbs
 --
 create bigfile tablespace servicios_transacciones_tbs
-    datafile '/unam/bda/proyecto/disks/d21/servicios_transacciones_01.dbf' size 1g
-    autoextend on next 100m maxsize 2g
+    datafile '/unam/bda/proyecto/disks/d21/servicios_transacciones_01.dbf' size 500m
+    autoextend on next 5m maxsize 750m
     extent management local autoallocate
     segment space management auto
 ;
